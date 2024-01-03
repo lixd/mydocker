3bcddd16476faa122474df7a285a66da0229ce5d
lixd <xueduan.li@gmail.com>
2024-01-03 15:29:11 +0800

    fix: make all mount point private

--- container/init.go
+++ container/init.go
@@ -15,6 +15,13 @@ import (
 */
 func RunContainerInitProcess(command string, args []string) error {
 	log.Infof("command:%s", command)
+
+	// systemd 加入linux之后, mount namespace 就变成 shared by default, 所以你必须显示声明你要这个新的mount namespace独立。
+	// 即 mount proc 之前先把所有挂载点的传播类型改为 private，避免本 namespace 中的挂载事件外泄。
+	syscall.Mount("", "/", "", syscall.MS_PRIVATE|syscall.MS_REC, "")
+	// 如果不先做 private mount，会导致挂载事件外泄，后续再执行 mydocker 命令时 /proc 文件系统异常
+	// 可以执行 mount -t proc proc /proc 命令重新挂载来解决
+	// ---分割线---
 	defaultMountFlags := syscall.MS_NOEXEC | syscall.MS_NOSUID | syscall.MS_NODEV
 	_ = syscall.Mount("proc", "/proc", "proc", uintptr(defaultMountFlags), "")
 	argv := []string{command}
